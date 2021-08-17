4cd372366fa62b8428e4eb70b31d7f4ce802d4d1
RHT <rodrigohisa@gmail.com>
2021-08-17 12:25:22 -0300

    DepartmentService

---
+++ src/model/services/DepartmentService.java
@@ -0,0 +1,21 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentService {
+	
+	public List<Department> findAll() {
+		// MOCK
+		List<Department> list = new ArrayList<>();
+		
+		list.add(new Department(1, "Books"));
+		list.add(new Department(2, "Fish"));
+		list.add(new Department(3, "Cars"));
+		
+		return list;
+		
+	}
+}
