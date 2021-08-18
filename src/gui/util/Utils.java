1fe5878240417cdeb4d226e7a82a847d2302fe7d
RHT <rodrigohisa@gmail.com>
2021-08-18 13:40:54 -0300

    Saving Sellers

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -92,4 +92,13 @@ public class Utils {
 			}
 		});
 	}
+	
+	
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
