29c8dcefa2870b0affb19b85082e456ee68f70e6
RHT <rodrigohisa@gmail.com>
2021-08-17 21:12:31 -0300

    Passing a department on Form

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,9 +9,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable{
 
+	private Department entity;
+	
 	@FXML
 	private TextField txtId;
 	
@@ -38,6 +41,9 @@ public class DepartmentFormController implements Initializable{
 		System.out.println("onBtCancelAction");
 	}
 
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
@@ -48,4 +54,17 @@ public class DepartmentFormController implements Initializable{
 		Constraints.setTextFieldDouble(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
 	}
+	
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null!");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
+	
+	
+	
+	
 }
